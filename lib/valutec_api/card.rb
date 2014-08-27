439c2aa8fb97233eb1174a8549fdb45d7a3dfca7
Nikky Southerland <nikky@ricksteves.com>
2014-08-27 13:37:12 -0700

    Spec out

--- lib/valutec_api/card.rb
+++ lib/valutec_api/card.rb
@@ -1,37 +1,84 @@
-module ValuetecApi
-   class Card
+require 'httparty'
 
-    def initialize(config_vars)
+module ValutecApi
+  class Card
+    include HTTParty
+
+    base_uri 'https://ws.valutec.net/Valutec.asmx'
+
+    attr_accessor :client_key, :terminal_id
+
+    def initialize(config_vars={})
+      @client_key = get_client_key(config_vars)
+      @terminal_id = get_terminal_id(config_vars)
     end
 
     def card_balance
+      response = make_api_call('/Transaction_CardBalance')
     end
 
     def add_value
+      response = make_api_call('/Transaction_AddValue')
     end
 
     def activate_card
+      response = make_api_call('/Transaction_ActivateCard')
     end
 
     def cash_out
+      response = make_api_call('/Transaction_CashOut')
     end
 
     def create_card
+      response = make_api_call('/Transaction_CreateCard')
     end
 
     def deactivate_card
+      response = make_api_call('/Transaction_DeactivateCard')
     end
 
     def host_totals
+      response = make_api_call('/Transaction_HostTotals')
     end
 
     def replace_card
+      response = make_api_call('/Transaction_ReplaceCard')
     end
 
     def sale
+      response = make_api_call('/Transaction_Sale')
     end
 
     def void
+      response = make_api_call('/Transaction_Void')
+    end
+
+    private
+
+    def make_api_call(method,params={})
+      params.merge!({"ClientKey" => client_key, "TerminalID" => terminal_id})
+      self.class.get(method,params)
+    end
+
+    def get_client_key(config_vars)
+      if config_vars[:client_key]
+        config_vars[:client_key]
+      elsif ENV['VALUTEC_CLIENT_KEY']
+        ENV['VALUTEC_CLIENT_KEY']
+      else
+        raise "ENV['VALUTEC_CLIENT_KEY'] not specified, nor is it included in class initialization"
+      end
+    end
+
+    def get_terminal_id(config_vars)
+      if config_vars[:terminal_id]
+        config_vars[:terminal_id]
+      elsif ENV['VALUTEC_TERMINAL_ID']
+        ENV['VALUTEC_TERMINAL_ID']
+      else
+        raise "ENV['VALUTEC_TERMINAL_ID'] not specified, nor is it included in class initialization"
+      end
+
     end
   end
 end
--- spec/valutec_api/card_spec.rb
+++ spec/valutec_api/card_spec.rb
@@ -1,8 +1,65 @@
 require 'spec_helper'
 
 describe ValutecApi::Card do
-  let (:card) {ValutecApi::Card.new}
-  it "does things" do
-    card
+  let (:valid_card) {ValutecApi::Card.new({card_number: '1234123412341234'})}
+  let (:invalid_card) {ValutecApi::Card.new({card_number: '6666666666666666'})}
+
+  it "raises if client_key and terminal_id are not specified" do
+    expect{ValutecApi::Card.new({client_key: '1234',terminal_id: '4321'})}.to_not raise_error
+    expect{ValutecApi::Card.new({client_key: '1234'})}.to raise_error
+    expect{ValutecApi::Card.new({terminal_id: '4321'})}.to raise_error
+    expect{ValutecApi::Card.new}.to raise_error
+
+    ENV['VALUTEC_TERMINAL_ID'] = "derp"
+    ENV['VALUTEC_CLIENT_KEY'] = "doop"
+
+    expect{ValutecApi::Card.new({client_key: '1234',terminal_id: '4321'})}.to_not raise_error
+    expect{ValutecApi::Card.new({client_key: '1234'})}.to_not raise_error
+    expect{ValutecApi::Card.new({terminal_id: '4321'})}.to_not raise_error
+    expect{ValutecApi::Card.new}.to_not raise_error
   end
+
+  context '#card_balance' do
+    it "returns card balance as a float if exists" do
+      expect(valid_card.card_balance).to eq 49.99
+    end
+
+    it "returns false if card not exist" do
+      # expect(.card_balance).to be_false
+    end
+  end
+
+  context '#add_value' do
+    it "returns new balance as float if successful"
+    it "returns false if unsuccessful"
+  end
+
+  context "#activate_card" do
+    it "returns things"
+  end
+
+  context "#cash_out" do
+    it "returns stuff"
+  end
+
+  context "#deactivate_card" do
+    it "derps"
+  end
+
+  context "#host_totals" do
+    it "?????"
+  end
+
+  context "#replace_card" do
+    it "who knows"
+  end
+
+  context "#sale" do
+    it "transaction-a-fies"
+  end
+
+  context "#void" do
+    it "hi"
+  end
+
 end
