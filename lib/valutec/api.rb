1dee7b3004984b3677731c94ee62ef93f707743c
Nikky Southerland <nikky@ricksteves.com>
2014-11-20 11:05:37 -0800

    Allow valutec variables to be defined in app

--- lib/valutec/api.rb
+++ lib/valutec/api.rb
@@ -11,9 +11,10 @@ module Valutec
     attr_accessor :client_key, :terminal_id, :server_id
 
     def initialize(config_vars={})
-      @client_key = ENV['VALUTEC_CLIENT_KEY'] || raise("ENV['VALUTEC_CLIENT_KEY'] not specified")
-      @terminal_id = ENV['VALUTEC_TERMINAL_ID'] || raise("ENV['VALUTEC_TERMINAL_ID'] not specified, nor is it included in class initialization")
-      @server_id = ENV['VALUTEC_SERVER_ID'] || raise("ENV['VALUTEC_SERVER_ID'] not specified, nor is it included in class initialization")
+
+      @client_key = Rails.application.config.try(:valutec_client_key) || ENV['VALUTEC_CLIENT_KEY'] || raise("ENV['VALUTEC_CLIENT_KEY'] not specified")
+      @terminal_id = Rails.application.config.try(:valutec_terminal_id) || ENV['VALUTEC_TERMINAL_ID'] || raise("ENV['VALUTEC_TERMINAL_ID'] not specified, nor is it included in class initialization")
+      @server_id = Rails.application.config.try(:valutec_server_id) || ENV['VALUTEC_SERVER_ID'] || raise("ENV['VALUTEC_SERVER_ID'] not specified, nor is it included in class initialization")
     end
 
     def call(method,params={})
@@ -32,7 +33,7 @@ module Valutec
       # I'm not convinced identifier is used for anything, but Valutec
       # complains when this param is missing. Generate random things to
       # shove into this field unless otherwise specified.
-      ENV.fetch('VALUTEC_IDENTIFIER', SecureRandom.hex(5))
+      Rails.application.config.try(:valutec_identifier) || ENV.fetch('VALUTEC_IDENTIFIER', SecureRandom.hex(5))
     end
   end
 end
